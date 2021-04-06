3b0ab3e3e1002a82aa072b7eeaae69f3d77652bb
Elman <58637039+magerramovvv@users.noreply.github.com>
2021-04-06 16:44:46 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+  "use strict";
+
+  // jQuery counterUp (used in Whu Us section)
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 3000
+  });
+
+})(jQuery);
+
